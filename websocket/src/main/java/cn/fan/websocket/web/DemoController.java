e0acc10dcbb0d376c885d712cf6677fecdeafdb8
LightMoney <fan85719223@outlook.com>
2020-02-28 12:06:02 +0800

    更新websocket项目

---
+++ websocket/src/main/java/cn/fan/websocket/web/DemoController.java
@@ -0,0 +1,36 @@
+package cn.fan.websocket.web;
+
+
+import cn.fan.websocket.core.WebSocketServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+/**
+ * WebSocketController
+ * @author fgy
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("index")
+    public ResponseEntity<String> index(){
+        return ResponseEntity.ok("请求成功");
+    }
+
+    @GetMapping("page")
+    public ModelAndView page(){
+        return new ModelAndView("websocket.html");
+    }
+
+    @RequestMapping("/push/{toUserId}")
+    public ResponseEntity<String> pushToWeb(String message, @PathVariable String toUserId) throws IOException {
+        WebSocketServer.sendInfo(message,toUserId);
+        return ResponseEntity.ok("MSG SEND SUCCESS");
+    }
+}
