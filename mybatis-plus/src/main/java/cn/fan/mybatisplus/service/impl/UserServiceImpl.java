8011b1526b0498ba3c461f667818dc0a3a0241aa
LightMoney <fan85719223@outlook.com>
2020-03-27 17:44:20 +0800

    mybatis-generator 生成实体类上swagger注解 根据数据库的标签

--- mybatis-plus/src/main/java/cn/fan/mybatisplus/service/impl/UserServiceImpl.java
+++ mybatis-plus/src/main/java/cn/fan/mybatisplus/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import cn.fan.mybatisplus.dao.UserMapper;
 import cn.fan.mybatisplus.entity.User;
 import cn.fan.mybatisplus.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
