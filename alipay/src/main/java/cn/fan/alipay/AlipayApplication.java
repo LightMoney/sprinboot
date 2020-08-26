63c576b9f76fbf07dcb5b3707b220cd504864435
LightMoney <fan85719223@outlook.com>
2020-08-26 09:00:14 +0800

    支付

---
+++ alipay/src/main/java/cn/fan/alipay/AlipayApplication.java
@@ -0,0 +1,13 @@
+package cn.fan.alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlipayApplication.class, args);
+    }
+
+}
