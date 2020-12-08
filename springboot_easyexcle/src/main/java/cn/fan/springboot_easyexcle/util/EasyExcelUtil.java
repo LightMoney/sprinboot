c993a8802e264f001bf0a56c4f53a65e2e1d10ca
LightMoney <fan85719223@outlook.com>
2020-12-08 10:14:24 +0800

    恢复注释的代码

--- springboot_easyexcle/src/main/java/cn/fan/springboot_easyexcle/util/EasyExcelUtil.java
+++ springboot_easyexcle/src/main/java/cn/fan/springboot_easyexcle/util/EasyExcelUtil.java
@@ -1,93 +1,93 @@
-//package cn.fan.springboot_easyexcle.util;
-//
-//
-//import com.alibaba.excel.ExcelReader;
-//import com.alibaba.excel.event.AnalysisEventListener;
-//import com.alibaba.excel.event.adapter.DefaultAnalysisEventListenerAdapter;
-//import com.alibaba.excel.metadata.BaseRowModel;
-//import com.alibaba.excel.metadata.Sheet;
-//import com.alibaba.excel.support.ExcelTypeEnum;
-//import com.alibaba.excel.util.StringUtils;
-//import lombok.extern.slf4j.Slf4j;
-//
-//import java.io.InputStream;
-//import java.util.List;
-//
-///**
-// * @ClassName EasyExcelUtil
-// * @Description easyExcel操作excel工具类
-// * @Auther William
-// * @Date 2019/6/5 11:35
-// * @Version 1.0
-// */
-//@Slf4j
-//public class EasyExcelUtil {
-//
-//
-//    /*
-//     *@Description: 读取Excel文件内容
-//     *@param in excel文件流
-//     *@param tClass 对应excel实体bean
-//     *@return: 对应excel实体bean的list
-//     *@Author:  William
-//     *@Date:  2019/6/5 13:24
-//     */
-//    public static <T> List<T> getExcelContent(InputStream in, Class<T> tClass, String fileName) {
-//        AnalysisEventListener<T> listener = new DefaultAnalysisEventListenerAdapter<>();
-//        return getExcelContent(in, tClass, fileName, listener, 1, 1);
-//    }
-//
-//
-//    /**
-//     * @param in       输入流
-//     * @param tClass   类信息
-//     * @param fileName 文件名称
-//     * @param listener 监听器  实现 AnalysisEventListener抽象方法 提供了默认的DefaultAnalysisEventListenerAdapter
-//     * @param <T>      数据结果类型
-//     * @param sheetNo  sheet页  从1开始
-//     * @param headLineNum 列头的行数量
-//     * @return 数据结果
-//     * @Description: 获取excel content 可传递自定义的监听listener
-//     */
-//    public static <T> List<T> getExcelContent(InputStream in, Class<T> tClass, String fileName,
-//                                                AnalysisEventListener<T> listener, int sheetNo, int headLineNum) {
-//        try {
-//            ExcelReader excelReader = new ExcelReader(in, getExcelTypeEnum(fileName), null, listener);
-//            // 第二个参数为表头行数，按照实际设置
-//            excelReader.read(new Sheet(sheetNo, headLineNum, (Class<? extends BaseRowModel>) tClass));
-//            listener.getData();
-//        } catch (Exception e) {
-//            log.warn("读取excel 异常", e);
-//        }
-//        return listener.getData();
-//    }
-//
-//
-//    /**
-//     * @Description  有默认的监听器 需要传入sheet页 头数量
-//     * @Param [in, tClass, fileName, sheetNo, headLineNum] 文件流,类信息,文件名称,sheet第几页从1开始,列头数量
-//     * @return 数据的集合
-//     **/
-//    public static <T> List<T> getExcelContent(InputStream in, Class<T> tClass, String fileName, int sheetNo, int headLineNum) {
-//        AnalysisEventListener<T> listener = new DefaultAnalysisEventListenerAdapter<>();
-//        getExcelContent(in, tClass, fileName, listener, sheetNo, headLineNum);
-//        return listener.getData();
-//    }
-//
-//
-//    /**
-//     * @param fileName 文件名称
-//     * @return excel类型枚举
-//     * @throws Exception 异常
-//     */
-//    private static ExcelTypeEnum getExcelTypeEnum(String fileName) throws Exception {
-//        if (StringUtils.isEmpty(fileName)) {
-//            throw new Exception("空文件名！");
-//        }
-//        if (fileName.endsWith(".xls")) {
-//            return ExcelTypeEnum.XLS;
-//        } else {
-//            return ExcelTypeEnum.XLSX;
-//        }
-//    }
-//}
+package cn.fan.springboot_easyexcle.util;
+
+
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.event.adapter.DefaultAnalysisEventListenerAdapter;
+import com.alibaba.excel.metadata.BaseRowModel;
+import com.alibaba.excel.metadata.Sheet;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import com.alibaba.excel.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @ClassName EasyExcelUtil
+ * @Description easyExcel操作excel工具类
+ * @Auther William
+ * @Date 2019/6/5 11:35
+ * @Version 1.0
+ */
+@Slf4j
+public class EasyExcelUtil {
+
+
+    /*
+     *@Description: 读取Excel文件内容
+     *@param in excel文件流
+     *@param tClass 对应excel实体bean
+     *@return: 对应excel实体bean的list
+     *@Author:  William
+     *@Date:  2019/6/5 13:24
+     */
+    public static <T> List<T> getExcelContent(InputStream in, Class<T> tClass, String fileName) {
+        AnalysisEventListener<T> listener = new DefaultAnalysisEventListenerAdapter<>();
+        return getExcelContent(in, tClass, fileName, listener, 1, 1);
+    }
+
+
+    /**
+     * @param in       输入流
+     * @param tClass   类信息
+     * @param fileName 文件名称
+     * @param listener 监听器  实现 AnalysisEventListener抽象方法 提供了默认的DefaultAnalysisEventListenerAdapter
+     * @param <T>      数据结果类型
+     * @param sheetNo  sheet页  从1开始
+     * @param headLineNum 列头的行数量
+     * @return 数据结果
+     * @Description: 获取excel content 可传递自定义的监听listener
+     */
+    public static <T> List<T> getExcelContent(InputStream in, Class<T> tClass, String fileName,
+                                                AnalysisEventListener<T> listener, int sheetNo, int headLineNum) {
+        try {
+            ExcelReader excelReader = new ExcelReader(in, getExcelTypeEnum(fileName), null, listener);
+            // 第二个参数为表头行数，按照实际设置
+            excelReader.read(new Sheet(sheetNo, headLineNum, (Class<? extends BaseRowModel>) tClass));
+            listener.getData();
+        } catch (Exception e) {
+            log.warn("读取excel 异常", e);
+        }
+        return listener.getData();
+    }
+
+
+    /**
+     * @Description  有默认的监听器 需要传入sheet页 头数量
+     * @Param [in, tClass, fileName, sheetNo, headLineNum] 文件流,类信息,文件名称,sheet第几页从1开始,列头数量
+     * @return 数据的集合
+     **/
+    public static <T> List<T> getExcelContent(InputStream in, Class<T> tClass, String fileName, int sheetNo, int headLineNum) {
+        AnalysisEventListener<T> listener = new DefaultAnalysisEventListenerAdapter<>();
+        getExcelContent(in, tClass, fileName, listener, sheetNo, headLineNum);
+        return listener.getData();
+    }
+
+
+    /**
+     * @param fileName 文件名称
+     * @return excel类型枚举
+     * @throws Exception 异常
+     */
+    private static ExcelTypeEnum getExcelTypeEnum(String fileName) throws Exception {
+        if (StringUtils.isEmpty(fileName)) {
+            throw new Exception("空文件名！");
+        }
+        if (fileName.endsWith(".xls")) {
+            return ExcelTypeEnum.XLS;
+        } else {
+            return ExcelTypeEnum.XLSX;
+        }
+    }
+}
--- springboot_easyexcle/src/test/java/cn/fan/springboot_easyexcle/SpringbootEasyexcleApplicationTests.java
+++ springboot_easyexcle/src/test/java/cn/fan/springboot_easyexcle/SpringbootEasyexcleApplicationTests.java
@@ -10,6 +10,7 @@ import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.event.AnalysisEventListener;
 //import com.alibaba.excel.event.adapter.DefaultAnalysisEventListenerAdapter;
+import com.alibaba.excel.event.adapter.DefaultAnalysisEventListenerAdapter;
 import com.alibaba.excel.metadata.Sheet;
 import com.alibaba.excel.metadata.Table;
 import com.alibaba.excel.support.ExcelTypeEnum;
@@ -146,27 +147,27 @@ public class SpringbootEasyexcleApplicationTests {
         }
     }
 
-//    @Test
-//    public void read() throws Exception {
-//        try (InputStream in = new FileInputStream("withHead.xlsx");) {
-////            AnalysisEventListener<List<String>> listener = new AnalysisEventListener<List<String>>() {
-////
-////                @Override
-////                public void invoke(List<String> object, AnalysisContext context) {
-////                    System.err.println("Row:" + context.getCurrentRowNum() + " Data:" + object);
-////                }
-////
-////                @Override
-////                public void doAfterAllAnalysed(AnalysisContext context) {
-////                    System.err.println("doAfterAllAnalysed...");
-////                }
-////            };
-//            AnalysisEventListener<List<String>> listener = new DefaultAnalysisEventListenerAdapter<>();
-//            ExcelReader excelReader = new ExcelReader(in, null, listener);
-//            excelReader.read();
-//            System.err.println("data:" + listener.getData());
-//        }
-//    }
+    @Test
+    public void read() throws Exception {
+        try (InputStream in = new FileInputStream("withHead.xlsx");) {
+//            AnalysisEventListener<List<String>> listener = new AnalysisEventListener<List<String>>() {
+//
+//                @Override
+//                public void invoke(List<String> object, AnalysisContext context) {
+//                    System.err.println("Row:" + context.getCurrentRowNum() + " Data:" + object);
+//                }
+//
+//                @Override
+//                public void doAfterAllAnalysed(AnalysisContext context) {
+//                    System.err.println("doAfterAllAnalysed...");
+//                }
+//            };
+            AnalysisEventListener<List<String>> listener = new DefaultAnalysisEventListenerAdapter<>();
+            ExcelReader excelReader = new ExcelReader(in, null, listener);
+            excelReader.read();
+            System.err.println("data:" + listener.getData());
+        }
+    }
 
     /**
      * 读表通过模型
@@ -174,30 +175,33 @@ public class SpringbootEasyexcleApplicationTests {
      * @throws Exception
      */
 //    @Test
-//    public void readModel() throws Exception {
-//        try (InputStream in = new FileInputStream("withHead.xlsx");) {
-////            AnalysisEventListener<ExcelPropertyIndexModel> listener = new AnalysisEventListener<ExcelPropertyIndexModel>() {
-////
-////                @Override
-////                public void invoke(ExcelPropertyIndexModel object, AnalysisContext context) {
-////                    System.err.println("Row:" + context.getCurrentRowNum() + " Data:" + object);
-////                }
-////
-////                @Override
-////                public void doAfterAllAnalysed(AnalysisContext context) {
-////                    System.err.println("doAfterAllAnalysed...");
-////                }
-////            };
-//            AnalysisEventListener<ExcelPropertyIndexModel> listener = new DefaultAnalysisEventListenerAdapter<>();
-//            ExcelReader excelReader = new ExcelReader(in, null, listener);
-//            // 第二个参数为表头行数，按照实际设置
-//            excelReader.read(new Sheet(1, 1, ExcelPropertyIndexModel.class));
-//        }
+    public void readModel() throws Exception {
+        try (InputStream in = new FileInputStream("withHead.xlsx");) {
+//            AnalysisEventListener<ExcelPropertyIndexModel> listener = new AnalysisEventListener<ExcelPropertyIndexModel>() {
 //
-//    }
+//                @Override
+//                public void invoke(ExcelPropertyIndexModel object, AnalysisContext context) {
+//                    System.err.println("Row:" + context.getCurrentRowNum() + " Data:" + object);
+//                }
+//
+//                @Override
+//                public void doAfterAllAnalysed(AnalysisContext context) {
+//                    System.err.println("doAfterAllAnalysed...");
+//                }
+//            };
+            AnalysisEventListener<ExcelPropertyIndexModel> listener = new DefaultAnalysisEventListenerAdapter<>();
+            ExcelReader excelReader = new ExcelReader(in, null, listener);
+            // 第二个参数为表头行数，按照实际设置
+            excelReader.read(new Sheet(1, 1, ExcelPropertyIndexModel.class));
+        }
+
+    }
 
     /**
      * 使用对应模板导出文档
+     * 注意sheetNO号要写上，否则无法写入
+     * 需要测试下打包后在服务器上路径是否正确
+     * 该方法适用于单sheet模板数据导入
      * @throws IOException
      */
     @Test
@@ -206,62 +210,61 @@ public class SpringbootEasyexcleApplicationTests {
 //            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("static/template2.xlsx");
 //        String templateFileName = this.getClass().getResource("/static/").getPath() + "template2.xlsx";
 
-        String templateFileName = TestFileUtil.getPath() + "static" + File.separator + "template2.xlsx";
-        // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
-        // {} 代表普通变量 {.} 代表是list的变量
-        // 这里模板 删除了list以后的数据，也就是统计的这一行
+    String templateFileName = TestFileUtil.getPath() + "static" + File.separator + "template2.xlsx";
+    // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
+    // {} 代表普通变量 {.} 代表是list的变量
+    // 这里模板 删除了list以后的数据，也就是统计的这一行
 //        String templateFileName =
-//                this.getPath() + "demo" + File.separator + "fill" + File.separator + "complexFillWithTable.xlsx";
-//
-        String fileName = TestFileUtil.getPath() + "static" + File.separator + "t2.xlsx";
+//        this.getPath() + "demo" + File.separator + "fill" + File.separator + "complexFillWithTable.xlsx";
+    String fileName = TestFileUtil.getPath() + "static" + File.separator + "t2.xlsx";
 
-
-        ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
+    ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
 //        我这里不写sheetNo会有读不到的问题
-        WriteSheet writeSheet = EasyExcel.writerSheet(1).build();
-        List<FillData> list = new ArrayList<>();
-        FillData fillData = new FillData();
-        fillData.setDate(new Date());
-        fillData.setCustomerName("测试");
-        fillData.setNum(11);
-        fillData.setProductName("测试产品");
-        fillData.setOne(13.0);
-        fillData.setTicketNo("15489");
-        fillData.setType("c");
-        fillData.setUnit("PCs");
-        fillData.setCountOne(22.0);
-        FillData fillData1 = new FillData();
-        fillData1.setDate(new Date());
-        fillData1.setCustomerName("测试1");
-        fillData1.setNum(11);
-        fillData1.setProductName("测试产品1");
-        fillData1.setOne(13.0);
-        fillData1.setTicketNo("154891");
-        fillData1.setType("c1");
-        fillData1.setUnit("PCs1");
-        fillData1.setCountOne(22.0);
-        list.add(fillData);
-        list.add(fillData1);
+    WriteSheet writeSheet = EasyExcel.writerSheet(1).build();
+    List<FillData> list = new ArrayList<>();
+    FillData fillData = new FillData();
+    fillData.setDate(new Date());
+    fillData.setCustomerName("测试");
+    fillData.setNum(11);
+    fillData.setProductName("测试产品");
+    fillData.setOne(13.0);
+    fillData.setTicketNo("15489");
+    fillData.setType("c");
+    fillData.setUnit("PCs");
+    fillData.setCountOne(22.0);
+    FillData fillData1 = new FillData();
+    fillData1.setDate(new Date());
+    fillData1.setCustomerName("测试1");
+    fillData1.setNum(11);
+    fillData1.setProductName("测试产品1");
+    fillData1.setOne(13.0);
+    fillData1.setTicketNo("154891");
+    fillData1.setType("c1");
+    fillData1.setUnit("PCs1");
+    fillData1.setCountOne(22.0);
+    list.add(fillData);
+    list.add(fillData1);
 // 这里注意 入参用了forceNewRow 代表在写入list的时候不管list下面有没有空行 都会创建一行，然后下面的数据往后移动。默认 是false，会直接使用下一行，如果没有则创建。
-        // forceNewRow 如果设置了true,有个缺点 就是他会把所有的数据都放到内存了，所以慎用
-        // 简单的说 如果你的模板有list,且list不是最后一行，下面还有数据需要填充 就必须设置 forceNewRow=true 但是这个就会把所有数据放到内存 会很耗内存
-        // 如果数据量大 list不是最后一行 参照下一个
-        FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
-        // 直接写入数据
-        excelWriter.fill(list, fillConfig, writeSheet);
-        excelWriter.fill(list, fillConfig, writeSheet);
+    // forceNewRow 如果设置了true,有个缺点 就是他会把所有的数据都放到内存了，所以慎用
+    // 简单的说 如果你的模板有list,且list不是最后一行，下面还有数据需要填充 就必须设置 forceNewRow=true 但是这个就会把所有数据放到内存 会很耗内存
+    // 如果数据量大 list不是最后一行 参照下一个
+    FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
+    // 直接写入数据
+    excelWriter.fill(list, fillConfig, writeSheet);
+    excelWriter.fill(list, fillConfig, writeSheet);
 
 //             写入list之前的数据
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("orderNo", "123456");
-        map.put("count", 1000);
-        excelWriter.fill(map, writeSheet);
+    Map<String, Object> map = new HashMap<String, Object>();
+    map.put("orderNo", "123456");
+    map.put("count", 1000);
+    excelWriter.fill(map, writeSheet);
 
 
-        excelWriter.finish();
-        // 总体上写法比较复杂 但是也没有想到好的版本 异步的去写入excel 不支持行的删除和移动，也不支持备注这种的写入，所以也排除了可以
-        // 新建一个 然后一点点复制过来的方案，最后导致list需要新增行的时候，后面的列的数据没法后移，后续会继续想想解决方案
+    excelWriter.finish();
+    // 总体上写法比较复杂 但是也没有想到好的版本 异步的去写入excel 不支持行的删除和移动，也不支持备注这种的写入，所以也排除了可以
+    // 新建一个 然后一点点复制过来的方案，最后导致list需要新增行的时候，后面的列的数据没法后移，后续会继续想想解决方案
 //        }
+
     }
 //下载
 //    @Override
