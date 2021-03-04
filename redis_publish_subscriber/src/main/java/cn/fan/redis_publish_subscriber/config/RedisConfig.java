b97ed656e8a246f30e73ef55a67327f1a68fa0ea
LightMoney <fan85719223@outlook.com>
2021-03-04 11:31:52 +0800

    test

--- redis_publish_subscriber/src/main/java/cn/fan/redis_publish_subscriber/config/RedisConfig.java
+++ redis_publish_subscriber/src/main/java/cn/fan/redis_publish_subscriber/config/RedisConfig.java
@@ -20,6 +20,11 @@ import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 当你在配置类(@Configuration)上使用@EnableCaching注解时，
+ * 会触发一个post processor，这会扫描每一个spring bean，
+ * 查看是否已经存在注解对应的缓存。如果找到了，就会自动创建一个代理拦截方法调用，使用缓存的bean执行处理
+ */
 @Configuration
 @EnableCaching
 public class RedisConfig {
